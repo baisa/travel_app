3d50cbab10586a8f6a57366007bb8b81cac6b2a7
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-09-23 10:49:23 +0200

    Completing CRUD for trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -17,8 +17,31 @@ class TripsController < ApplicationController
   end
 
   def create
-    @trip = Trip.new(params.require(:trip).permit(:title, :description))
-    @trip.save
-    redirect_to @trip
+    @trip = Trip.new(trip_params)
+    if @trip.save
+     redirect_to @trip
+   else
+      render 'new'
+    end
+  end
+
+  def update
+    @trip = Trip.find(params[:id])
+    if @trip.update(trip_params)
+      redirect_to @trip
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @trip = Trip.find(params[:id])
+    @trip.destroy
+    redirect_to trips_path
+  end
+
+  private
+  def trip_params
+    params.require(:trip).permit(:title, :description)
   end
 end
