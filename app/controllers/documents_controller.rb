4339403ca438190faee9bc9d4b7933b915aca340
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-10-02 18:05:37 +0200

    CRUD for documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,2 +1,34 @@
 class DocumentsController < ApplicationController
+
+  def index
+    @trip = Trip.find(params[:trip_id])
+    @documents = @trip.documents.all
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+    @document = @trip.documents.find(params[:id])
+  end
+
+  def new
+    @document = Document.new
+  end
+
+  def create
+    @trip = Trip.find(params[:trip_id])
+    @document = @trip.documents.create(document_params)
+    if @document.save
+      redirect_to trip_documents_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def document_params
+    params.require(:document).permit(:title)
+  end
+
+
 end
