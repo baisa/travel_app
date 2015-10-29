042170ab9a6afcdba584f85861996e1987362538
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-10-29 16:13:33 +0100

    session controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
      redirect_to @user, notice: "Thank you for signing up!"
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  resources :sessions
   resources :users
   get 'main/index'
   resources :trips do
