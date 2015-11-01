8380f130a73192091d3de180fc1aac283d003990
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-11-01 19:14:48 +0100

    fixing log in form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter do
+    puts "We are in #{params[:controller]}.#{params[:action]}"
+  end
+
 private
 
   def current_user 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: 'Logged in!'
     else
-      flash.now.alert = 'Email or password is invalid'
+      flash[:notice] = 'Email or password is invalid'
       render 'new'
     end
   end
