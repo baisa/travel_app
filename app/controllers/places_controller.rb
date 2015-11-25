5d9e6eaedd46a1cc6c2ca2427620e389ce913054
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-11-25 22:55:07 +0100

    routing for place model

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -28,7 +28,7 @@ class PlacesController < ApplicationController
 
     respond_to do |format|
       if @place.save
-        format.html { redirect_to @place, notice: 'Place was successfully created.' }
+        format.html { redirect_to trip_places_path, notice: 'Place was successfully created.' }
         format.json { render :show, status: :created, location: @place }
       else
         format.html { render :new }
