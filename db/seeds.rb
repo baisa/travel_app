9969abb673867c4446e2d88eeb9675b779a62fac
Barbara Slawinska <slawinskabarbara@gmail.com>
2016-01-02 19:57:02 +0100

    preparing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+trips = Trip.create([{title: 'Norway', description: 'great outdoor trip'},
+  {title: 'Korfu', description: 'easy sunny vacation'},
+  {title: 'Switzerland', description: 'skiing in Laax resort'}
+  ])
+
+trips.each do |trip|
+  trip.todos.create([{task: 'flight reservations'}, {task: 'change currency'}, {task: 'book a hotel'}])
+end
