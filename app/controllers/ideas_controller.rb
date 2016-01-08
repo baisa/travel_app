37488b4c19d04274a238d34c65901fca102652be
Barbara Slawinska <slawinskabarbara@gmail.com>
2016-01-08 14:43:12 +0100

    controller for ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,28 @@
 class IdeasController < ApplicationController
+  layout "left_menu"
+  
+  def index
+    @trip = Trip.find(params[:trip_id])
+    @ideas = @trip.ideas.all
+    @idea = Idea.new
+  end
+
+  def create
+    @trip = Trip.find(params[:trip_id])
+    @idea = @trip.ideas.create(idea_params)
+    redirect_to trip_ideas_path(@trip)
+  end
+
+  def destroy
+    @trip = Trip.find(params[:trip_id])
+    @idea = @trip.ideas.find(params[:id])
+    @idea.destroy
+    redirect_to trip_ideas_path(@trip)
+  end
+
+  private
+    def idea_params
+      params.require(:idea).permit(:idea, :planned_at)
+    end
+
 end
