0f08e184e7d0071026841011e02decaf602a4b20
Barbara Slawinska <slawinskabarbara@gmail.com>
2016-01-19 11:47:38 +0100

    adding generate_token method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
   has_many :user_trips
   has_many :trips, through: :user_trips
+  before_create { generate_token(:auth_token) }
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
 end
