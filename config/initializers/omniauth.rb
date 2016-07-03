2023ca1c35596ede8bc8bc6b0e772dd0d9f696de
Barbara Slawinska <slawinskabarbara@gmail.com>
2016-07-03 22:50:56 +0100

    dotenv for env variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   
-  provider :twitter, 'EnPDBBOmEHM27nDboiw56HRxu', 'FoK3KEWC6NEYYE1jFJgFsIV8Gn1XKD8FNLu4ezIhyGZiKXszKv'
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
